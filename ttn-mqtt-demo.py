37a95a3986df099156c6908e9e042a46e93a28b7
Highlights_gao <104325796+highlights-gao@users.noreply.github.com>
2023-12-11 13:02:26 +0100

    Update ttn-mqtt-demo.py
    delete private information on ttn

--- ttn-mqtt-demo.py
+++ ttn-mqtt-demo.py
@@ -7,8 +7,8 @@ import logging
 import base64
 import csv
 ###
-User = "lorawan-reklamation@ttn"
-Password = "NNSXS.Y7HN3T6DDDLKCVPCEKTXXWP3M5UKWZJPI2U3MAY.SV4ZAYABRRYGBP3SFJRFU4HJNDYWLUE3JTQFFY75THQJCM7ETYLQ"
+User = "your username"
+Password = "your password"
 PUBLIC_TLS_ADDRESS = "eu1.cloud.thethings.network"
 PUBLIC_TLS_ADDRESS_PORT = "8883"
 theRegion = "EU1"
